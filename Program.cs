695f9c8d8044c42c7b4523fd48c3fbb4cc426448
Pedro Atalaia <pedrocondatalaia@hotmail.com>
2022-03-01 17:26:57 -0300

    start

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using Entities;
+
+namespace aula225_Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            // ---- List to recive the names -----//
+
+            List<string> result = list.Select(NameUpper).ToList();
+            foreach(string res in result)
+            {
+                Console.WriteLine(res);
+            }
+        }
+
+        static string NameUpper(Product product)
+        {
+            return product.Name.ToUpper();
+        }
+    }
+}
